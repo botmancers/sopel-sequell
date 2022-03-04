4e3b6e34914e2266110320c9016777b5da4cebf5
Joanna Doyle <jdoyle@epages.com>
2022-03-04 01:50:17 +0100

    add sender nick to cmd, reply to pms in pm

--- sopel_modules/sopel_sequell/sopel_sequell.py
+++ sopel_modules/sopel_sequell/sopel_sequell.py
@@ -1,53 +1,85 @@
 # coding=utf-8
 
-from sopel import module
+import re
+from sopel import plugin
 from sopel.config.types import (
     StaticSection, ValidatedAttribute
 )
 from sopel.tools import get_logger
 
-sequell_rule = r'^\$sq\$(.*)$'
+cmd = 'sq'
+nick_sub_cmd_list = [
+    'dump',
+    'lg',
+    'gamesby',
+]
 logger = get_logger(__name__)
 
 class SequellSection(StaticSection):
-    relay_prefix = ValidatedAttribute('relay_prefix')
-    relay_nick = ValidatedAttribute('relay_nick')
-    relay_chan = ValidatedAttribute('relay_chan')
+    cmd = ValidatedAttribute('cmd')
+    sq_nick = ValidatedAttribute('sq_nick')
+    to_chan = ValidatedAttribute('to_chan')
 
 
 def configure(config):
     config.define_section('sequell', SequellSection)
     config.sequell.configure_setting(
-        'relay_prefix',
-        'Prefix for commands to relay (will be stripped)'
+        'cmd',
+        'Name of Sequell relay command (default: sq)'
     )
     config.sequell.configure_setting(
-        'relay_nick',
-        'Nick for sequell the actual bot'
+        'sq_nick',
+        'Nick for Sequell the actual bot',
     )
     config.sequell.configure_setting(
-        'relay_chan',
-        'Channel for botspam (sequell replies)'
+        'to_chan',
+        'Channel for botspam (sequell replies)',
     )
 
 
 def setup(bot):
     bot.config.define_section('sequell', SequellSection)
-    sequell_rule = rf"^{bot.config.sequell.relay_prefix}(.*)$"
+    if bot.config.sequell.cmd is not None:
+         cmd = bot.config.sequell.cmd
+    bot.memory['sequell'] = []
 
 
 # Match all messages except for those which start with common bot command
 # prefixes
-@module.rule(sequell_rule)
+@plugin.command(cmd)
 def sequell_cmd(bot, trigger):
-    logger.info(f"{trigger.nick} sent cmd {trigger.match.group(1)}")
-    bot.say(f"{trigger.match.group(1)}", bot.config.sequell.relay_nick)
+    pattern = re.compile(r"""^
+    ([&!\?]+)   # sequell prefix
+    (\w+)       # sequell cmd
+    (?:\s+
+      (\w+)     # trailing parameters
+    )*
+$""", re.X)
+    logger.debug(f"<{trigger.nick}> {trigger.match.string}")
+    match = pattern.match(trigger.match.group(2))
+    if match is None:
+        return
+    bot.memory['sequell'].append({
+        'from': trigger.nick,
+        'pm': trigger.is_privmsg,
+        'prefix': match.group(1),
+        'cmd': match.group(2),
+        'params': match.group(3),
+    })
+    logger.debug(f"cmd matched: {match.group(1)}, {match.group(2)}, {match.group(3)}")
+    msg_to_sequell = f"{match.group(0)}"
+    if match.group(3) is None:
+        msg_to_sequell += f" {trigger.nick}"
+    bot.say(msg_to_sequell, bot.config.sequell.sq_nick)
 
 
-@module.require_privmsg
-@module.rule(r'.*')
+@plugin.require_privmsg
+@plugin.rule(r'.*')
 def sequell_reply(bot, trigger):
-    logger.info(f"got a pm from {trigger.nick}")
-    if trigger.nick == bot.config.sequell.relay_nick:
+    if trigger.nick == bot.config.sequell.sq_nick:
         logger.info(f"{trigger.nick} replied {trigger.match.string}")
-        bot.say(trigger.match.string, bot.config.sequell.relay_chan)
+        last_cmd = bot.memory['sequell'].pop(-1)
+        if last_cmd['pm']:
+            bot.say(trigger.match.string, last_cmd['from'])
+        else:
+            bot.say(trigger.match.string, bot.config.sequell.to_chan)
